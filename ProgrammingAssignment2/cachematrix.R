99d7ea69583cb507682fc8cc484795ab59db8bc9
BRodas <rodas6180@gmail.com>
2016-04-03 00:39:08 -0300

    my assignment

---
+++ ProgrammingAssignment2/cachematrix.R
@@ -0,0 +1,36 @@
+## In this script there are two function, the first one is to save on cache
+## some info, and the second one is to solve info on the first one.
+
+## This function save info on cache
+
+makeCacheMatrix <- function(x = matrix()) {
+
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+## This other function solve info about the first one.
+
+cacheSolve <- function(x, ...) {
+  
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  ## Return a matrix that is the inverse of 'x'
+  m
+}
